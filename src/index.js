0e0061c17e335b7c2e778345dfa22b5fe301af54
Giovanna Cunha <giovannakelli@gmail.com>
2021-03-10 02:23:07 -0300

    add ThemeProvider with lightTheme to App.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import lightTheme from './themes/lightTheme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
