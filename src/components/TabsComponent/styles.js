1925dd1eeb6d02f508cb70dc1995e775742e60f8
Giovanna Cunha <giovannakelli@gmail.com>
2021-04-01 21:02:30 -0300

    add filter by category functionality to home page component

--- src/components/TabsComponent/styles.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-import { makeStyles } from '@material-ui/styles';
-
-const useStyles = makeStyles((lightTheme) => ({
-
-}));
-
-export default useStyles;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,37 +1,79 @@
+/* eslint-disable max-len */
 /* eslint-disable no-useless-return */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-underscore-dangle */
-import { Box, Grid, Toolbar } from '@material-ui/core';
+import {
+  AppBar, Box, Grid, Tab, Tabs, Toolbar,
+} from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CardComponent } from '../../components/CardComponent';
-import { TabsComponent } from '../../components/TabsComponent';
 import { api } from '../../services/api';
 
 export const Home = () => {
   const [restaurants, setRestaurants] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const [selectedTab, setSelectedTab] = useState(0);
 
   useEffect(() => {
-    const fetchAllRestaurants = async () => {
-      try {
-        const response = await api.get('/');
-        setRestaurants(response.data.restaurants);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchAllRestaurants();
+    try {
+      const fetchAllRestaurants = async () => {
+        try {
+          const response = await api.get('/');
+          setRestaurants(response.data.restaurants);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchAllRestaurants();
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
+
+  useEffect(() => {
+    try {
+      const fetchCategories = async () => {
+        const response = await api.get('/restaurant_categories/');
+        setCategory(response.data.restaurantCategories);
+      };
+
+      fetchCategories();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const handleSelectedTab = (e, newValue) => {
+    setSelectedTab(newValue);
+  };
+
+  const filterRestaurants = (tab) => {
+    const filteredRestaurants = restaurants.filter((item) => item.restaurantCategory.name === tab);
+    setFilter(filteredRestaurants);
+  };
 
   return (
     <>
-      <TabsComponent restaurants={restaurants} />
+      <Toolbar variant="regular" />
+      <AppBar position="static" color="primary">
+        <Tabs
+          variant="scrollable"
+          value={selectedTab}
+          scrollButtons="auto"
+          textColor="secondary"
+          onChange={handleSelectedTab}
+        >
+          {category.map((cat) => (<Tab key={cat._id} onClick={() => filterRestaurants(cat.name)} label={cat.name} />))}
 
+        </Tabs>
+      </AppBar>
       <Toolbar />
       <Box>
         <Grid container spacing={2}>
-          {restaurants.map((restaurant) => (
-            <Grid item align="center" xs={12} sm={6} md={6} lg={4} xl={3}>
+          {filter.map((restaurant) => (
+            <Grid item align="center" xs={12} sm={6} md={6} lg={4} xl={3} key={restaurant._id}>
               <Link to={`/details/${restaurant._id}`} style={{ textDecoration: 'none' }}>
                 <CardComponent key={restaurant._id} restaurant={restaurant} />
               </Link>
