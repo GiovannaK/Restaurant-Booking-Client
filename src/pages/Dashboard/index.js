fe3f0a67f25c7af19b0798fa00415667f5f5bfda
Giovanna Cunha <giovannakelli@gmail.com>
2021-04-06 02:13:55 -0300

    feat: Register partner form has been validated

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-unused-vars */
 import {
-  AppBar, Tab, Tabs, Toolbar,
+  AppBar, Box, Tab, Tabs, Toolbar,
 } from '@material-ui/core';
-import { TabPanel } from '@material-ui/lab';
 import React, { useState } from 'react';
 import useProfile from '../../context/ProfileContext/hooks/useProfile';
 import { ProfileContext } from '../../context/ProfileContext/profileContext';
@@ -30,12 +29,29 @@ export const Dashboard = () => {
           textColor="secondary"
           onChange={handleSelectedTab}
         >
-          <Tab label="Minhas reservas" />
-          <Tab label="Meus dados" />
+          {!user.isPartner ? (
+            <div>
+              <Tab label="Minhas reservas" />
+              <Tab label="Meus dados" />
+            </div>
+
+          ) : (
+            <div>
+              <Tab label="Restaurantes Cadastrados" />
+              <Tab label="Meus dados" />
+            </div>
+          )}
         </Tabs>
       </AppBar>
-      {selectedTab === 0 && <UserBookings />}
-      {selectedTab === 1 && <UserProfile />}
+      <Box>
+        {selectedTab === 0 && <UserBookings />}
+      </Box>
+      <Box>
+        {selectedTab === 1 && <UserProfile />}
+      </Box>
+      <Box>
+        {selectedTab === 2 && 'Restaurantes'}
+      </Box>
     </>
   );
 };
--- src/pages/RegisterPartner/index.js
+++ src/pages/RegisterPartner/index.js
@@ -2,13 +2,70 @@
 import {
   Box, Button, Card, CardContent, Grid, Hidden, TextField, Toolbar, Typography,
 } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { isEmail, isMobilePhone } from 'validator';
 import partnerImage from '../../images/partner.svg';
+import { api } from '../../services/api';
 import useStyles from './styles';
 
 export const RegisterPartner = () => {
   const classes = useStyles();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      header: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    let formErrors = false;
+
+    if (firstName.length < 3 || firstName.length > 255) {
+      formErrors = true;
+      toast.error('Primeiro nome precisa ter entre 3 e 50 caracteres');
+    }
+
+    if (lastName.length < 3 || lastName.length > 255) {
+      formErrors = true;
+      toast.error('Último nome precisa ter entre 3 e 50 caracteres');
+    }
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('E-mail inválido');
+    }
+
+    if (!isMobilePhone(String(phone), 'pt-BR')) {
+      formErrors = true;
+      toast.error('Número de celular inválido, verifique se o código de área foi digitado ');
+    }
+
+    if (password.length < 8 || password.length > 255) {
+      formErrors = true;
+      toast.error('Senha deve ter pelo menos 8 caracteres');
+    }
+
+    if (formErrors) return;
+    try {
+      const { data } = await api.post('/session/register_partners',
+        {
+          firstName, lastName, email, password, phone,
+        }, config);
+      toast.info(`Enviamos um e-mail de ativação para ${email}`);
+    } catch (error) {
+      toast.error('Já existe uma conta associada a este e-mail');
+    }
+  };
+
   return (
     <>
       <Toolbar />
@@ -34,13 +91,15 @@ export const RegisterPartner = () => {
                   conta para que possamos vincular seu restaurante e
                   entrar em contato.
                 </Typography>
-                <form>
+                <form onSubmit={handleSubmit}>
                   <TextField
                     id="outlined-firstName-input"
                     label="Nome"
                     type="text"
                     placeholder="Seu nome"
                     variant="outlined"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
                     style={{ width: '100%' }}
                     InputLabelProps={{
                       shrink: true,
@@ -54,19 +113,8 @@ export const RegisterPartner = () => {
                     type="text"
                     placeholder="Seu sobrenome"
                     variant="outlined"
-                    style={{ width: '100%' }}
-                    InputLabelProps={{
-                      shrink: true,
-                      className: classes.inputLabel,
-                    }}
-                  />
-                  <Toolbar />
-                  <TextField
-                    id="outlined-phone-input"
-                    label="Número de celular"
-                    type="number"
-                    placeholder="exemplo 11999000000"
-                    variant="outlined"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
                     style={{ width: '100%' }}
                     InputLabelProps={{
                       shrink: true,
@@ -81,6 +129,8 @@ export const RegisterPartner = () => {
                     placeholder="Seu e-mail"
                     autoComplete="current-email"
                     variant="outlined"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     style={{ width: '100%' }}
                     InputLabelProps={{
                       shrink: true,
@@ -95,6 +145,23 @@ export const RegisterPartner = () => {
                     placeholder="Sua senha..."
                     autoComplete="current-password"
                     variant="outlined"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    style={{ width: '100%' }}
+                    InputLabelProps={{
+                      shrink: true,
+                      className: classes.inputLabel,
+                    }}
+                  />
+                  <Toolbar />
+                  <TextField
+                    id="outlined-phone-input"
+                    label="Número de celular"
+                    type="number"
+                    placeholder="exemplo 11999000000"
+                    variant="outlined"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
                     style={{ width: '100%' }}
                     InputLabelProps={{
                       shrink: true,
